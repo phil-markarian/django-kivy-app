966301196318d8d26999701276a7873791a4c072
phil-markarian <phil.markarian@gmail.com>
2023-11-27 18:28:10 +0900

    update path for ToAPI urls.py

--- TodoAPI/urls.py
+++ TodoAPI/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.all_tasks, name='all_tasks'),
-    path('create', views.create_task, name='create_task')
+    path('admin/', admin.site.urls),
+    path('', include('tasks.urls'))
 ]
