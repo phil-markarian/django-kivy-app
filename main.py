5e893a39ebee35cce439fdb3326a973dc5854753
phil-markarian <phil.markarian@gmail.com>
2023-11-27 19:45:43 +0900

    MyRecycleView added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+from kivy.clock import Clock
+from kivy.uix.recycleview import RecycleView
 from kivy.app import App
 
 from kivy.uix.screenmanager import ScreenManager
@@ -17,6 +20,20 @@ class ScreenManagement(ScreenManager):
 #app class
 class TodoApp(App):
     pass
+#recycle view for home screen
+class MyRecycleView(RecycleView):
+    def __init__(self, **kwargs):
+        super(MyRecycleView, self).__init__(**kwargs)
+        self.load_data()
+        Clock.schedule_interval(self.load_data, 1)
+    def load_data(self, *args):
+        store = requests.get('http://127.0.0.1:8000/').json()
+
+        list_data = []
+        for item in store:
+            list_data.append({'text': item['name']})
+
+        self.data = list_data
 
 if __name__ == '__main__':
     TodoApp().run()
