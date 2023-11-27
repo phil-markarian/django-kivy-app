4e0d624411756039381dff78acb1ae172059b4df
phil-markarian <phil.markarian@gmail.com>
2023-11-27 19:04:16 +0900

    kivy file add

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.boxlayout import BoxLayout
+
+#menu
+class Menu(BoxLayout):
+    pass
+#Screen Management
+class ScreenManagement(ScreenManager):
+    pass
+#app class
+class TodoApp(App):
+    pass
+
+if __name__ == '__main__':
+    TodoApp().run()
