ee5ac34cb08a7c41caab90713f89303f53d46599
phil-markarian <phil.markarian@gmail.com>
2023-11-27 18:10:15 +0900

    serliazer.py add

---
+++ tasks/serializer.py
@@ -0,0 +1,9 @@
+## about serializers: https://www.django-rest-framework.org/api-guide/serializers/
+
+from rest_framework import serializers
+from .models import *
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        field = '__all__'
