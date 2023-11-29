d3dce51d4536cb628d863cfb354ad987c0662776
phil-markarian <phil.markarian@gmail.com>
2023-11-29 15:24:07 +0900

    format of tasks to match users API. uses APIView

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_tasks, name='all_tasks'),
-    path('create', views.create_task, name='create_task')
+    path('', views.AllTasksView.as_view(), name='all_tasks'),
+    path('create/', views.CreateTaskView.as_view(), name='create_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,24 +1,21 @@
-from django.shortcuts import render
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
-from .serializers import *
-from .models import *
+class AllTasksView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['GET'])
-def all_tasks(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-def create_task(request):
-    data = request.data
-    serializer = TaskSerializer(data=data)
-    if serializer.is_valid():
-        serializer.save()
-        tasks = Task.objects.all()  # Retrieve all tasks after creation
-        all_tasks_serializer = TaskSerializer(tasks, many=True)
-        return Response(all_tasks_serializer.data, status=status.HTTP_200_OK)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class CreateTaskView(APIView):
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            tasks = Task.objects.all()  # Retrieve all tasks after creation
+            all_tasks_serializer = TaskSerializer(tasks, many=True)
+            return Response(all_tasks_serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
