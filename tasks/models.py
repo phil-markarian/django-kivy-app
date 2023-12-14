4974cc48bba17205edb7ab69010099bcea2c65f8
phil-markarian <phil.markarian@gmail.com>
2023-12-14 20:36:50 +0900

    added user foreign key to tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Task(models.Model):
-    name = models.CharField(max_length=200)
-    def __str__(self):
-        return self.name
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1) 
+    name = models.CharField(max_length=255, default='')
+    description = models.TextField(default='')
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+
+class StoreItem(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
