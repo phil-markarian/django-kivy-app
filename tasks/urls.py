9f37d3b552b9798202a8605af315fea3c96283cc
phil-markarian <phil.markarian@gmail.com>
2023-12-19 22:08:43 +0900

    updated tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import AllTasksView, CreateTaskView, StoreItemList, StoreItemDetail
 
 urlpatterns = [
-    path('', views.AllTasksView.as_view(), name='all_tasks'),
+    path('all_tasks/', views.AllTasksView.as_view(), name='all_tasks'),
     path('create/', views.CreateTaskView.as_view(), name='create_task'),
+    path('store/', StoreItemList.as_view(), name='store-list'),
+    path('store/<int:pk>/', StoreItemDetail.as_view(), name='store-detail'),
 ]
