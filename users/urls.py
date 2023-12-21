c305aba664ef3e573c0e16da5c2b5caef36cdd2c
phil-markarian <phil.markarian@gmail.com>
2023-12-21 16:00:54 +0900

    made it so that redirect doesn't happen automatically

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.UserRegistrationView.as_view(), name='user_register'),
-    path('login/', views.UserLoginView.as_view(), name='user_login'),
-    path('profile/', views.UserProfileView.as_view(), name='user_profile'),
+    path('register', views.UserRegistrationView.as_view(), name='user_register'),
+    path('login', views.UserLoginView.as_view(), name='user_login'),
+    path('profile', views.UserProfileView.as_view(), name='user_profile'),
     # Add other user-related URLs as needed (e.g., logout, password reset, etc.)
 ]
