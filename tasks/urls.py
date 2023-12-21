2af9154c5f61b4beacaa36305e7d795421529d79
phil-markarian <phil.markarian@gmail.com>
2023-12-21 16:43:28 +0900

    fix redirect issue.

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,8 +3,8 @@ from . import views
 from .views import AllTasksView, CreateTaskView, StoreItemList, StoreItemDetail
 
 urlpatterns = [
-    path('all_tasks/', views.AllTasksView.as_view(), name='all_tasks'),
-    path('create/', views.CreateTaskView.as_view(), name='create_task'),
-    path('store/', StoreItemList.as_view(), name='store-list'),
-    path('store/<int:pk>/', StoreItemDetail.as_view(), name='store-detail'),
+    path('all_tasks', views.AllTasksView.as_view(), name='all_tasks'),
+    path('create', views.CreateTaskView.as_view(), name='create_task'),
+    path('store', StoreItemList.as_view(), name='store-list'),
+    path('store/<int:pk>', StoreItemDetail.as_view(), name='store-detail'),
 ]
